09e52b6cb08eefd3d2416ddc7ce129bba57bbd02
Aatish Bhatia <aatish@gmail.com>
2019-04-30 17:27:37 -0400

    Update rotating.js

--- js/rotating.js
+++ js/rotating.js
@@ -41,7 +41,7 @@ function init() {
     // makeSound();
     listener = new THREE.AudioListener();
     audio = new THREE.Audio( listener );
-    mediaElement = new Audio('../media/lucky_dragons.mp3');
+    mediaElement = new Audio('./../media/lucky_dragons.mp3');
     mediaElement.loop = true;
     mediaElement.play();
     console.log("Playing2");
