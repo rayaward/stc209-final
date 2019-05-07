6da41228742f7dcc9dbd099e8af1fc721484423b
rayaward <50107655+rayaward@users.noreply.github.com>
2019-05-06 21:38:31 -0400

    Fix bugs in (~)

--- js/objects.js
+++ js/objects.js
@@ -7,7 +7,7 @@
             emissive: '#006063',
             shininess: 10
         }
-        var sphereMaterial = new THREE.MeshPhongMaterial(matProps);≈
+        var sphereMaterial = new THREE.MeshPhongMaterial(matProps);
         var sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
         sphereMesh.castShadow = true;
         sphereMesh.position.y = 0.75 * Math.PI / 2;
